68d632d0a8e7c1fffb6431707874f74382e1de43
anuragbhati01 <anuragbhatiofficial@gmail.com>
2025-10-05 12:51:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const navDialog = document.getElementById('nav-dialog');
+function handleMenu(){
+    navDialog.classList.toggle('hidden');
+}
+
+const initialTranslateLTR = -48*4;
+const initialTranslateRTL = 36*4;
+
+function setupIntersectionObserver(element, isLTR, speed){
+    const intersectionCallback = (entries) => {
+        const isIntersecting = entries[0].isIntersecting;
+        if(isIntersecting){
+            document.addEventListener('scroll', scrollhandler);
+        }
+        else{
+            document.removeEventListener('scroll', scrollhandler);
+        }
+    }
+    const intersectionObserver = new IntersectionObserver(intersectionCallback);
+    intersectionObserver.observe(element);
+
+    function scrollhandler(){
+        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+        let totalTranslate = 0;
+        if(isLTR){
+            totalTranslate = initialTranslateLTR + translateX;
+        }
+        else{
+            totalTranslate = -(initialTranslateRTL + translateX);
+        }
+        element.style.transform = `translateX(${totalTranslate}px)`;
+    }
+}
+
+
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+const line4 = document.getElementById('line4');
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
+setupIntersectionObserver(line4, true, 0.80);
+
+const dtElements = document.querySelectorAll('dt');
+
+dtElements.forEach(elments => {
+    elments.addEventListener('click', () => {
+        const ddId = elments.getAttribute('aria-controls');
+        const ddElement = document.getElementById(ddId);
+        const ddArrowIcon = elments.querySelectorAll('i')[0];
+
+        ddElement.classList.toggle('hidden');
+        ddArrowIcon.classList.toggle('-rotate-180');
+    });
+});
